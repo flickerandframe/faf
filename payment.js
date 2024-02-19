ffd7a9eef27096481a81835cf87a11dbf14876c3
flickerandframe <140784836+flickerandframe@users.noreply.github.com>
2024-02-19 14:17:02 -0500

    Add files via upload

--- payment.js
+++ payment.js
@@ -11,9 +11,10 @@ document.addEventListener("DOMContentLoaded", function() {
 
     // Get the 'amount' query parameter value
     var amount = getQueryParam('amount');
-    
+    var id = getQueryParam('invoiceid');
+    var paymentType = getQueryParam('payment');
     // Update the content of the div with id 'amount' and the Venmo link
-    if(amount !== null) {
+    if(amount !== null && id !== null && paymentType !== null) {
 
         
         // Update the Venmo link (This example assumes you have a specific URL format for Venmo payments)
@@ -21,5 +22,34 @@ document.addEventListener("DOMContentLoaded", function() {
         // You will need to replace this with your actual payment URL or mechanism.
         var venmoLink = document.getElementById('venmoLink');
         venmoLink.href = "https://account.venmo.com/payment-link?amount="+ amount +"&note=Photoshoot&recipients=flickerandframe&txn=pay"; // Placeholder URL
+
+        var paragraph = document.getElementById('paragraphUpdate');
+        if (paragraph) {
+            if(paymentType == 'downpayment') {
+                paragraph.innerHTML = "We currently do not accept direct card payments.<br>We do cash and checks, but make sure you email us before hand.<br>" +
+                    "Invoice ID: " + id + "<br>" +
+                    "<br>It appears you are paying your downpayment (25%)!<br>You must pay this before or the day of the shoot.<br>" +
+                    "<br>If you have any questions, please reach out to us!<br>" +
+                    "Amount Due: $" + amount + "<br>"
+            } else if (paymentType == 'full'){
+                paragraph.innerHTML = "We currently do not accept direct card payments.<br>We do cash and checks, but make sure you email us before hand.<br>" +
+                    "Invoice ID: " + id + "<br>" +
+                    "<br>It appears you are paying your full amount!<br>You must pay this before or the day of the shoot, or you can use the link in your email to pay the downpayment.<br>" +
+                    "<br>If you have any questions, please reach out to us!<br>" +
+                    "Amount Due: $" + amount + "<br>"
+            } else if (paymentType == 'downpaid'){
+                paragraph.innerHTML = "We currently do not accept direct card payments.<br>We do cash and checks, but make sure you email us before hand.<br>" +
+                    "Invoice ID: " + id + "<br>" +
+                    "<br>It appears you have paid your downpayment!<br>You must pay the additional 75% in a 30 day timeframe after the photoshoot.<br>" +
+                    "<br>If you have any questions, please reach out to us!<br>" +
+                    "Amount Due: $" + amount + "<br>"
+            } else {
+                window.location.href = './'
+            }
+        }
+
+
+    } else {
+        window.location.href = './'
     }
 });
